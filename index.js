86b83d71786f6721bac31442a30b196f68ad59af
Tiffany Crooks <tiffcrooks2011@gmail.com>
2023-04-24 22:34:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+// Code your solution
+function findMatching (drivers, string) {
+    return drivers.filter(el => el.toLowerCase() === string.toLowerCase());
+  }
+
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(driver => driver.startsWith(string))
+  
+}
+
+function matchName(drivers, string){
+  return drivers.filter(driver => driver.name === string)
+}
